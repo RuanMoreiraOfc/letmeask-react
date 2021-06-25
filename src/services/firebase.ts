d607a1a4cdc80d3fa783439211b40c2cc50a448d
Ruan Moreira de Jesus <ruanmoreiraofc@hotmail.com>
2021-06-25 03:09:45 -0300

    Sort config by alphabetical order

--- src/services/firebase.ts
+++ src/services/firebase.ts
@@ -3,19 +3,23 @@ import firebase from 'firebase/app';
 import 'firebase/auth';
 import 'firebase/database';
 
-export { auth, database };
+export {
+    firebase
+    , auth
+    , database
+};
 
 const env = Object.entries( process.env ).filter( e => e[0].startsWith('REACT_APP_FIREBASE') ).map( e => e[1] );
 
 const firebaseConfig = {
     apiKey: env[0]
-    , authDomain: env[1]
-    , databaseURL: env[2]
-    , projectId: env[3]
-    , storageBucket: env[4]
+    , appId: env[1]
+    , authDomain: env[2]
+    , databaseURL: env[3]
+    , measurementId: env[4]
     , messagingSenderId: env[5]
-    , appId: env[6]
-    , measurementId: env[7]
+    , projectId: env[6]
+    , storageBucket: env[7]
 };
 
 firebase.initializeApp( firebaseConfig );
