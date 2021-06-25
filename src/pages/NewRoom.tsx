1706271eea3e077e1d16a6d585995cbcb7ca911e
Ruan Moreira de Jesus <ruanmoreiraofc@hotmail.com>
2021-06-25 03:14:33 -0300

    Create New Room Page

---
+++ src/pages/NewRoom.tsx
@@ -0,0 +1,42 @@
+import styles from '../styles/auth.module.scss';
+
+import illustrationImg from '../assets/icons/illustration.svg';
+import logoImg from '../assets/icons/logo.svg';
+
+import { Link } from 'react-router-dom';
+
+import Button from '../components/Button';
+
+export default NewRoom;
+
+function NewRoom() {
+   const {
+      containerBox
+      , contentBox
+      , newRoomBox
+   } = styles;
+
+   return (
+      <div className={ containerBox }>
+         <aside>
+            <img src={ illustrationImg } alt="Ilustração simbolizando pergunta em respostas" />
+            <strong>Crie salas de Q&amp;A ao-vivo</strong>
+            <p>Tire as dúvidas da sua audiência em tempo real</p>
+         </aside>
+         <main>
+            <div className={ `${contentBox} ${newRoomBox}` }>
+               <img src={ logoImg } alt="Letmeask" />
+               <h2>Criar uma nova sala</h2>
+               <form>
+                  <input
+                     type="text"
+                     placeholder="Digite o código da sala"
+                  />
+                  <Button type="submit">Criar sala</Button>
+                  <p>Deseja entrar em uma sala existente? <Link to="/">clique aqui</Link></p>
+               </form>
+            </div>
+         </main>
+      </div>
+   );
+}
