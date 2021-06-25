71245b889da5bbcbb0b7a4c60a8e212ea3af7324
Ruan Moreira de Jesus <ruanmoreiraofc@hotmail.com>
2021-06-25 03:11:20 -0300

    Create Commom Button Component

---
+++ src/components/Button/index.tsx
@@ -0,0 +1,17 @@
+import styles from './styles.module.scss';
+
+import { ButtonHTMLAttributes } from 'react';
+
+export default Button;
+
+type ButtonProps = ButtonHTMLAttributes<HTMLButtonElement>;
+
+function Button( { className, ...rest }: ButtonProps ) {
+   return (
+      <button
+         type="button"
+         className={ styles.commomBtn + ( className ? ` ${className}` : '' ) }
+         { ...rest }
+      />
+   );
+}
