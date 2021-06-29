bc75dd4f52514aaeda76a7cbd30b9ada86a49def
Ruan Moreira de Jesus <ruanmoreiraofc@hotmail.com>
2021-06-29 05:31:09 -0300

    Add Admin Room Route

--- src/App.tsx
+++ src/App.tsx
@@ -7,6 +7,7 @@ import { AuthContextProvider } from './contexts/AuthContext';
 import Home from './pages/Home';
 import NewRoom from './pages/NewRoom';
 import Room from './pages/Room';
+import AdminRoom from './pages/AdminRoom';
 
 export default App;
 
@@ -18,6 +19,7 @@ function App() {
                <Route path="/" exact component={ Home } />
                <Route path="/rooms/new" component={ NewRoom } />
                <Route path="/rooms/:id" component={ Room } />
+               <Route path="/admin/rooms/:id" component={ AdminRoom } />
                <Redirect to="/"/>
             </Switch>
          </AuthContextProvider>
