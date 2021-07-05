5c30b0df7b1c3340a83d5d5db8f93414328f63da
Ruan Moreira de Jesus <ruanmoreiraofc@hotmail.com>
2021-07-04 22:26:48 -0300

    Update Logo on HeaderRoom Component

--- src/components/HeaderRoom/index.tsx
+++ src/components/HeaderRoom/index.tsx
@@ -5,6 +5,7 @@ import logoImg from '../../assets/icons/logo.svg';
 import { ReactNode } from 'react';
 import { Link } from 'react-router-dom';
 
+import Logo from '../Logo';
 import ButtonRoomCode from '../ButtonRoomCode';
 import ButtonSignOut from '../ButtonSignOut';
 
@@ -16,12 +17,15 @@ type HeaderRoomProps = {
 }
 
 function HeaderRoom( { roomCode, children }: HeaderRoomProps ) {
-   const { containerBox } = styles;
+   const {
+      containerBox
+      , logoBox
+   } = styles;
 
    return (
       <header className={ containerBox }>
          <section>
-            <Link to='/'><img src={logoImg} alt="Letmeask"/></Link>
+            <Link to='/'><Logo className={ logoBox } /></Link>
             <div>
                <ButtonRoomCode code={ roomCode } />
                { children }
