3cfff58b95620cc06b70f8af5c5b2585ef4c1f69
Ruan Moreira de Jesus <ruanmoreiraofc@hotmail.com>
2021-07-07 02:38:35 -0300

    Use DarkModeSwitch component on App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import './styles/global.scss';
 
+import { Fragment } from 'react';
 import { BrowserRouter, Switch, Route, Redirect } from 'react-router-dom';
 
 import { AuthContextProvider } from './contexts/AuthContext';
@@ -9,20 +10,25 @@ import NewRoom from './pages/NewRoom';
 import Room from './pages/Room';
 import AdminRoom from './pages/AdminRoom';
 
+import DarkModeSwitch from './components/DarkModeSwitch';
+
 export default App;
 
 function App() {
    return (
-      <BrowserRouter>
-         <AuthContextProvider>
-            <Switch>
-               <Route path="/" exact component={ Home } />
-               <Route path="/rooms/new" component={ NewRoom } />
-               <Route path="/rooms/:id" component={ Room } />
-               <Route path="/admin/rooms/:id" component={ AdminRoom } />
-               <Redirect to="/"/>
-            </Switch>
-         </AuthContextProvider>
-      </BrowserRouter>
+      <Fragment>
+         <DarkModeSwitch />
+         <BrowserRouter>
+            <AuthContextProvider>
+               <Switch>
+                  <Route path="/" exact component={ Home } />
+                  <Route path="/rooms/new" component={ NewRoom } />
+                  <Route path="/rooms/:id" component={ Room } />
+                  <Route path="/admin/rooms/:id" component={ AdminRoom } />
+                  <Redirect to="/"/>
+               </Switch>
+            </AuthContextProvider>
+         </BrowserRouter>
+      </Fragment>
    );
 }
